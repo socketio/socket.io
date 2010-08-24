70759ed53231430436fbeae349e79efe8c3f2cea
Guillermo Rauch <rauchg@gmail.com>
2010-08-23 21:20:55 -0700

    Support for automatic JSON encoding/decoding Simplified chat example to take advantage of JSON encoding/decoding Removed fs sync call from example

--- example/server.js
+++ example/server.js
@@ -25,8 +25,10 @@ server = http.createServer(function(req, res){
 				try {
 					var swf = path.substr(-4) === '.swf';
 					res.writeHead(200, {'Content-Type': swf ? 'application/x-shockwave-flash' : ('text/' + (path.substr(-3) === '.js' ? 'javascript' : 'html'))});
-					res.write(fs.readFileSync(__dirname + path, swf ? 'binary' : 'utf8'), swf ? 'binary' : 'utf8');
-					res.end();
+					fs.readFile(__dirname + path, swf ? 'binary' : 'utf8', function(err, data){
+						if (!err) res.write(data, swf ? 'binary' : 'utf8');
+						res.end();
+					});
 				} catch(e){ 
 					send404(res); 
 				}
@@ -42,22 +44,21 @@ server.listen(8080);
 		
 // socket.io, I choose you
 // simplest chat application evar
-var buffer = [], 
-		json = JSON.stringify,
-		io = io.listen(server);
+var io = io.listen(server),
+		buffer = [];
 		
 io.on('connection', function(client){
-	client.send(json({ buffer: buffer }));
-	client.broadcast(json({ announcement: client.sessionId + ' connected' }));
+	client.send({ buffer: buffer });
+	client.broadcast({ announcement: client.sessionId + ' connected' });
 
 	client.on('message', function(message){
 		var msg = { message: [client.sessionId, message] };
 		buffer.push(msg);
 		if (buffer.length > 15) buffer.shift();
-		client.broadcast(json(msg));
+		client.broadcast(msg);
 	});
 
 	client.on('disconnect', function(){
-		client.broadcast(json({ announcement: client.sessionId + ' disconnected' }));
+		client.broadcast({ announcement: client.sessionId + ' disconnected' });
 	});
 });
