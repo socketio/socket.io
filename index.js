4bec17e903be14bf801058b8990c565356c2512a
Guillermo Rauch <rauchg@gmail.com>
2010-08-24 17:17:48 -0700

    Make it possible to just require 'socket.io'

--- index.js
+++ index.js
@@ -1,4 +1,1 @@
-exports.Listener = require('./lib/socket.io/listener');
-exports.listen = function(server, options){
-	return new exports.Listener(server, options);
-};
+module.exports = require('./lib/socket.io');
