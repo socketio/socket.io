4465d3416b93913cf663ae4ac295fbcdb8476744
Guillermo Rauch <rauchg@gmail.com>
2010-10-03 15:40:06 -0300

    Tabs => spaces

--- tests/transports.websocket.js
+++ tests/transports.websocket.js
@@ -1,35 +1,35 @@
 var io = require('./../'),
-		Listener = io.Listener,
-		Client = require('./../lib/socket.io/client'),
-		WebSocket = require('./support/node-websocket-client/lib/websocket').WebSocket;
+    Listener = io.Listener,
+    Client = require('./../lib/socket.io/client'),
+    WebSocket = require('./support/node-websocket-client/lib/websocket').WebSocket;
 
 module.exports = {
-	
-	'test connection and handshake': function(assert){
-		var server = require('http').createServer(function(){}), sio, client, clientCount, close;
-		server.listen(8081);
+  
+  'test connection and handshake': function(assert){
+    var server = require('http').createServer(function(){}), sio, client, clientCount, close;
+    server.listen(8081);
 
-		sio = io.listen(server, {log: function(){}});
-		client;
-		clientCount = 0;
-		close = function(){
-			client.close();
-			server.close();
-			assert.ok(clientCount, 1);
-		};
-		
-		server.listen(port, function(){
-			client = new WebSocket('ws://localhost:8081/socket.io/websocket', 'borf');
-			client.onmessage = function(){
-				console.log('test');
-			};
-		});
-		
-		sio.on('connection', function(client){
-			console.log('test');
-			clientCount++;
-			assert.ok(client instanceof Client);
-		});
-	}
-	
+    sio = io.listen(server, {log: function(){}});
+    client;
+    clientCount = 0;
+    close = function(){
+      client.close();
+      server.close();
+      assert.ok(clientCount, 1);
+    };
+    
+    server.listen(port, function(){
+      client = new WebSocket('ws://localhost:8081/socket.io/websocket', 'borf');
+      client.onmessage = function(){
+        console.log('test');
+      };
+    });
+    
+    sio.on('connection', function(client){
+      console.log('test');
+      clientCount++;
+      assert.ok(client instanceof Client);
+    });
+  }
+  
 };
