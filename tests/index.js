6712d71a2e525bf9ac1c303a227405da52e1297c
Guillermo Rauch <rauchg@gmail.com>
2010-10-18 18:05:32 -0300

    Replaced empty with null in log option Comma first style for client serving tests

--- tests/index.js
+++ tests/index.js
@@ -1,15 +1,14 @@
 var io = require('socket.io')
   , Listener = io.Listener
   , Client = require('socket.io/client')
-  , WebSocket = require('../support/node-websocket-client/lib/websocket').WebSocket
-  , empty = function(){};
+  , WebSocket = require('../support/node-websocket-client/lib/websocket').WebSocket;
 
 module.exports = {
 
   'test server initialization': function(assert){
     var server = require('http').createServer(function(){}), sio;
     server.listen(8080);
-    sio = io.listen(server, {log: empty});
+    sio = io.listen(server, { log: null });
     assert.ok(sio instanceof Listener);
     setTimeout(function(){
       server.close();
@@ -17,14 +16,14 @@ module.exports = {
   },
   
   'test serving static javascript client': function(assert){
-    var server = require('http').createServer(function(){}), 
-        sio = io.listen(server, {log: empty});
-    assert.response(server,
-      { url: '/socket.io/socket.io.js' },
-      { body: /setPath/, headers: { 'Content-Type': 'text/javascript' }});
-    assert.response(server,
-      { url: '/socket.io/lib/vendor/web-socket-js/WebSocketMain.swf' },
-      { headers: { 'Content-Type': 'application/x-shockwave-flash' }});
+    var server = require('http').createServer(function(){})
+      , sio = io.listen(server, { log: null });
+    assert.response(server
+     , { url: '/socket.io/socket.io.js' }
+     , { body: /setPath/, headers: { 'Content-Type': 'text/javascript' }});
+    assert.response(server
+     , { url: '/socket.io/lib/vendor/web-socket-js/WebSocketMain.swf' }
+     , { headers: { 'Content-Type': 'application/x-shockwave-flash' }});
   }
 
 };
