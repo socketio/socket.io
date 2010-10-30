8e1bf87604aa368aeb3eddd046196252b1b6a203
Guillermo Rauch <rauchg@gmail.com>
2010-10-30 19:59:57 -0300

    Removed incorrect client

--- support/socket.io-client/lib/io.js
+++
@@ -1,9 +0,0 @@
-this.io = {
-  version: 0.1,
-	setPath: function(path){
-		this.path = path;		
-		WebSocket.__swfLocation = path + 'lib/vendor/web-socket-js/WebSocketMain.swf';
-	}
-};
-
-if ('jQuery' in this) jQuery.io = this.io;
