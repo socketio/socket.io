b98f9ceb07a199545122cfa164f3da696464aa7d
Guillermo Rauch <rauchg@gmail.com>
2010-11-15 02:34:53 -0300

    XHR-Polling tests adapted to new message encoding/decoding

--- tests/transports.xhr-polling.js
+++ tests/transports.xhr-polling.js
@@ -2,13 +2,9 @@ var io = require('socket.io')
   , http = require('http')
   , querystring = require('querystring')
   , port = 7400
-  , encode = require('socket.io/utils').encode
-  , decode = require('socket.io/utils').decode
   , Polling = require('socket.io/transports/xhr-polling');
 
-function server(callback){
-  return http.createServer(function(){});
-};
+require('socket.io/tests');
 
 function listen(s, callback){
   s._port = port;
@@ -71,19 +67,22 @@ module.exports = {
 
     listen(_server, function(){
       get(client(_server), '/socket.io/xhr-polling', function(data){
-        var sessid = decode(data);
-        assert.ok(Object.keys(_socket.clients).length == 1);
-        assert.ok(sessid == Object.keys(_socket.clients)[0]);
-        assert.ok(_socket.clients[sessid] instanceof Polling);
-        
-        _socket.clients[sessid].send('from server');
-        
-        get(client(_server), '/socket.io/xhr-polling/' + sessid, function(data){
-          assert.ok(decode(data), 'from server');
-          --trips || _server.close();
+        decode(data, function(sessid){
+          assert.ok(Object.keys(_socket.clients).length == 1);
+          assert.ok(sessid == Object.keys(_socket.clients)[0]);
+          assert.ok(_socket.clients[sessid] instanceof Polling);
+          
+          _socket.clients[sessid].send('from server');
+          
+          get(client(_server), '/socket.io/xhr-polling/' + sessid, function(data){
+            decode(data, function(msg){
+              assert.ok(msg[0] === 'from server');
+              --trips || _server.close();
+            });
+          });
+          
+          post(client(_server), '/socket.io/xhr-polling/' + sessid + '/send', {data: encode('from client')});
         });
-        
-        post(client(_server), '/socket.io/xhr-polling/' + sessid + '/send', {data: encode('from client')});
       });
     });
   },
@@ -114,21 +113,24 @@ module.exports = {
       
     listen(_server, function(){
       get(client(_server), '/socket.io/xhr-polling', function(data){
-        var sessid = decode(data);
-        assert.ok(_socket.clients[sessid]._open === false);
-        assert.ok(_socket.clients[sessid].connected);
-        _socket.clients[sessid].send('from server');
-        get(client(_server), '/socket.io/xhr-polling/' + sessid, function(data){
-          var durationCheck;
-          assert.ok(decode(data) == 'from server');
-          setTimeout(function(){
-            assert.ok(_socket.clients[sessid]._open);
-            assert.ok(_socket.clients[sessid].connected);
-            durationCheck = true;
-          }, 100);
-          get(client(_server), '/socket.io/xhr-polling/' + sessid, function(){
-            assert.ok(durationCheck);
-            _server.close();
+        decode(data, function(sessid){
+          assert.ok(_socket.clients[sessid]._open === false);
+          assert.ok(_socket.clients[sessid].connected);
+          _socket.clients[sessid].send('from server');
+          get(client(_server), '/socket.io/xhr-polling/' + sessid, function(data){
+            var durationCheck;
+            decode(data, function(msg){
+              assert.ok(msg[0] === 'from server');
+              setTimeout(function(){
+                assert.ok(_socket.clients[sessid]._open);
+                assert.ok(_socket.clients[sessid].connected);
+                durationCheck = true;
+              }, 100);
+              get(client(_server), '/socket.io/xhr-polling/' + sessid, function(){
+                assert.ok(durationCheck);
+                _server.close();
+              });
+            });
           });
         });
       });
