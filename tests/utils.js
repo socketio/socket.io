f33b0909f6fddee658a02643af717e4483a93b77
Tj Holowaychuk <tj@vision-media.ca>
2011-04-21 14:45:19 -0700

    added failing data decoding test

--- tests/utils.js
+++ tests/utils.js
@@ -10,6 +10,13 @@ module.exports = {
     assert.equal(decoded[1], '123456789');
   },
   
+  'test decoding of invalid data length recovery': function(){
+    var decoded = decode('~m~5~m~hello there' + '~m~5~m~again');
+    assert.equal(decoded.length, 2);
+    assert.equal(decoded[0], 'hello');
+    assert.equal(decoded[1], 'again');
+  },
+  
   'test decoding of bad framed messages': function(){
     var decoded = decode('~m~5~m~abcde' + '~m\uffsdaasdfd9~m~1aaa23456789');
     assert.equal(decoded.length, 1);
