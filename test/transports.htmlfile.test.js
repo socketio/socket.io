fcd474e89e16acd71368ef34da3c719d2c71f02f
Guillermo Rauch <rauchg@gmail.com>
2011-05-22 21:10:20 -0700

    Added htmlfile test for undeliverable volatile messages.

--- test/transports.htmlfile.test.js
+++ test/transports.htmlfile.test.js
@@ -176,6 +176,49 @@ module.exports = {
         }
       });
     });
+  },
+
+  'test sending undeliverable volatile messages': function (done) {
+    var port = ++ports
+      , cl = client(port)
+      , io = create(cl)
+      , messaged = false
+      , s;
+
+    io.configure(function () {
+      io.set('close timeout', 0);
+    });
+
+    io.sockets.on('connection', function (socket) {
+      s = socket;
+
+      socket.on('disconnect', function () {
+        messaged.should.be.false;
+        io.server.close();
+        done();
+      });
+    });
+
+    cl.handshake(function (sid) {
+      cl.data('/socket.io/{protocol}/htmlfile/' + sid, function () { });
+
+      setTimeout(function () {
+        cl.end();
+
+        setTimeout(function () {
+          s.volatile.send('wooooot');
+          cl = client(port);
+          cl.data('/socket.io/{protocol}/htmlfile/' + sid, function (msgs) {
+            if (msgs && msgs.length)
+              messaged = true;
+          });
+
+          setTimeout(function () {
+            cl.end();
+          }, 10);
+        }, 10);
+      }, 10);
+    });
   }
 
 };
