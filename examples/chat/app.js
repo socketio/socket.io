aeb8832c1d55574a50e6c89d773606dfaa7d0ba3
Arnout Kazemier <info@3rd-Eden.com>
2011-06-19 21:42:53 +0200

    Only emit discconect if the user actually has a nickname

--- examples/chat/app.js
+++ examples/chat/app.js
@@ -87,7 +87,10 @@ io.sockets.on('connection', function (socket) {
 
   socket.on('disconnect', function () {
     delete nicknames[socket.nickname];
-    socket.broadcast.emit('announcement', socket.nickname + ' disconnected');
-    socket.broadcast.emit('nicknames', nicknames);
+
+    if (socket.nickname) {
+      socket.broadcast.emit('announcement', socket.nickname + ' disconnected');
+      socket.broadcast.emit('nicknames', nicknames);
+    }
   });
 });
