039eac7b8381e77b8a7d54d398f2e2fa164b9405
Arnout Kazemier <info@3rd-Eden.com>
2011-06-26 23:57:52 +0200

    Added access to the auth data from the namespace authentications

--- test/namespace.test.js
+++ test/namespace.test.js
@@ -18,6 +18,30 @@ var sio = require('socket.io')
  */
 
 module.exports = {
+  'namespace pass no authentication': function (done) {
+    var cl = client(++ports)
+      , io = create(cl)
+      , ws;
+
+    io.of('/a')
+      .on('connection', function (socket) {
+        cl.end();
+        ws.finishClose();
+        io.server.close()
+        done();
+      });
+
+    cl.handshake(function (sid) {
+      ws = websocket(cl, sid);
+      ws.on('open', function () {
+        ws.packet({
+            type: 'connect'
+          , endpoint: '/a'
+        });
+      })
+    });
+  },
+
   'namespace pass authentication': function (done) {
     var cl = client(++ports)
       , io = create(cl)
@@ -70,14 +94,50 @@ module.exports = {
 
       ws.on('message', function (data) {
         if (data.endpoint == '/a') {
-          data.type.should.be.eql('error');
-          data.reason.should.be.eql('unauthorized')
+          data.type.should.eql('error');
+          data.reason.should.eql('unauthorized')
           
           cl.end();
           ws.finishClose();
           io.server.close()
           done();
         }
+      })
+    });
+  },
+
+  'namespace proxy data authentication': function (done) {
+    var cl = client(++ports)
+      , io = create(cl)
+      , ws;
+
+    io.of('/a')
+      .authorization(function (data, fn) {
+        data.foo = 'bar';
+        fn(null, true);
+      })
+      .on('connection', function (socket) {
+        var data = socket.data;
+
+        data.protocol.should.eql(sio.protocol);
+        data.foo.should.eql('bar');
+        data.transport.should.eql('websocket');
+        (!data.request).should.be.true;
+        (!!data.headers).should.be.true;
+
+        cl.end();
+        ws.finishClose();
+        io.server.close()
+        done();
+      });
+
+    cl.handshake(function (sid) {
+      ws = websocket(cl, sid);
+      ws.on('open', function () {
+        ws.packet({
+            type: 'connect'
+          , endpoint: '/a'
+        });
       })
     });
   }
