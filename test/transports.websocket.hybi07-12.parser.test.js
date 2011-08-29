0a6c78cbb82aca37f3e7e575c3fe1c11234cb921
einaros <einaros@gmail.com>
2011-08-29 07:56:40 +0200

    joined compatible hybi protocol handlers and updated test reference

--- test/transports.websocket.hybi07-12.parser.test.js
+++ test/transports.websocket.hybi07-12.parser.test.js
@@ -3,7 +3,7 @@
  */
 
 var assert = require('assert'); 
-var Parser = require('../lib/transports/websocket/7.js').Parser;
+var Parser = require('../lib/transports/websocket/hybi-07-12.js').Parser;
 
 /**
  * Returns a Buffer from a "ff 00 ff"-type hex string.
