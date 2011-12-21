2113a27fcab1e5a05cd2c25689c66b751b6683ef
Guillermo Rauch <rauchg@gmail.com>
2011-12-20 22:24:24 -0800

    Added common requirements for tests.
    This also adds a layer of compatibility for browser tests.

---
+++ test/common.js
@@ -0,0 +1,8 @@
+
+/*!
+ * Common test dependencies.
+ */
+
+// expose the globals that are obtained through `<script>` on the browser
+expect = require('expect.js')
+eio = require('../lib/engine.io-client')
