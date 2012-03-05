f049aa45bfaf0015d920d91243592999575518fa
Mickele Moriconi <mickele.moriconi@gmail.com>
2012-03-05 21:32:03 +0100

    Updating prettify and removing unused files

--- support/prettify/lang-proto.js
+++
@@ -1,1 +0,0 @@
-PR.registerLangHandler(PR.sourceDecorator({keywords:"bool bytes default double enum extend extensions false fixed32 fixed64 float group import int32 int64 max message option optional package repeated required returns rpc service sfixed32 sfixed64 sint32 sint64 string syntax to true uint32 uint64",cStyleComments:true}),["proto"])
