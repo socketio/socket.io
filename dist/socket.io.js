ecd752a7ea4b1d4ad46c2acde4e8f257f0a1ff76
Guillermo Rauch <rauchg@gmail.com>
2012-03-13 10:52:32 -0300

    Release 0.9.2

--- dist/socket.io.js
+++ dist/socket.io.js
@@ -1,4 +1,4 @@
-/*! Socket.IO.js build:0.9.1-1, development. Copyright(c) 2011 LearnBoost <dev@learnboost.com> MIT Licensed */
+/*! Socket.IO.js build:0.9.2, development. Copyright(c) 2011 LearnBoost <dev@learnboost.com> MIT Licensed */
 
 /**
  * socket.io
@@ -22,7 +22,7 @@
    * @api public
    */
 
-  io.version = '0.9.1-1';
+  io.version = '0.9.2';
 
   /**
    * Protocol implemented.
@@ -1714,7 +1714,7 @@
         });
       }
 
-      connect();
+      connect(self.options.transports);
 
       self.once('connect', function (){
         clearTimeout(self.connectTimeoutTimer);
