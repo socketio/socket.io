85df087cedd4e180917f50b3c43c4c4d7c006e2b
Stepan Filatov <stepanf@microsoft.com>
2012-07-27 20:41:31 +0400

    Make client compatible with AMD loaders. Issue #338.

--- bin/builder.js
+++ bin/builder.js
@@ -85,12 +85,16 @@ var baseTransports = {
 
 /**
  * Wrappers for client-side usage.
- * This enables usage in both top-level browser window and client-side CommonJS systems.
+ * This enables usage in top-level browser window, client-side CommonJS systems and AMD loaders.
  * If doing a node build for server-side client, this wrapper is NOT included.
  * @api private
  */
 var wrapperPre = "\nvar io = ('undefined' === typeof module ? {} : module.exports);\n(function() {\n";
-var wrapperPost = "\n})();";
+
+var wrapperPost = "\nif (typeof define === \"function\" && define.amd) {" +
+                  "\n  define(\"socket.io\", [], function () { return io; });" +
+                  "\n}\n})();";
+
 
 /**
  * Builds a custom Socket.IO distribution based on the transports that you
@@ -197,7 +201,9 @@ var builder = module.exports = function () {
       }
 
       // post-wrapper for non-server-side builds
-      if (!settings.node)code += wrapperPost;
+      if (!settings.node) {
+          code += wrapperPost;
+      }
 
       code = activeXObfuscator(code);
 
