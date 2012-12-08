2da017fde89b0507de7505708d21bfb7ab9b87a2
Guillermo Rauch <rauchg@gmail.com>
2012-12-08 20:09:24 -0300

    test: remove `version`

--- test/engine.io-client.js
+++ test/engine.io-client.js
@@ -1,10 +1,6 @@
 
 describe('engine.io-client', function () {
 
-  it('should expose version number', function () {
-    expect(eio.version).to.match(/[0-9]+\.[0-9]+\.[0-9]+/);
-  });
-
   it('should expose protocol number', function () {
     expect(eio.protocol).to.be.a('number');
   });
