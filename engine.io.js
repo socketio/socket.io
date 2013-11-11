b1714a9877bdb7663155ff4c1e8865c7cf4310d4
Guillermo Rauch <rauchg@gmail.com>
2013-11-11 17:59:29 -0300

    engine.io: updated build to fix WebSocket constructor issue

--- engine.io.js
+++ engine.io.js
@@ -2930,9 +2930,49 @@ module.exports = function(arr, obj){
   return -1;
 };
 },{}],22:[function(require,module,exports){
-var global=typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {};/// shim for browser packaging
 
-module.exports = global.WebSocket || global.MozWebSocket;
+/**
+ * Module dependencies.
+ */
+
+var global = (function() { return this; })();
+
+/**
+ * WebSocket constructor.
+ */
+
+var WebSocket = global.WebSocket || global.MozWebSocket;
+
+/**
+ * Module exports.
+ */
+
+module.exports = WebSocket ? ws : null;
+
+/**
+ * WebSocket constructor.
+ *
+ * The third `opts` options object gets ignored in web browsers, since it's
+ * non-standard, and throws a TypeError if passed to the constructor.
+ * See: https://github.com/einaros/ws/issues/227
+ *
+ * @param {String} uri
+ * @param {Array} protocols (optional)
+ * @param {Object) opts (optional)
+ * @api public
+ */
+
+function ws(uri, protocols, opts) {
+  var instance;
+  if (protocols) {
+    instance = new WebSocket(uri, protocols);
+  } else {
+    instance = new WebSocket(uri);
+  }
+  return instance;
+}
+
+if (WebSocket) ws.prototype = WebSocket.prototype;
 
 },{}]},{},[1])
 (1)
