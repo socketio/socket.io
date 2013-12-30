2f880c62645a42ecbaeeb59f03e3b06aa4ba70fd
Guillermo Rauch <rauchg@gmail.com>
2013-12-30 15:30:35 -0300

    test: style

--- test/support/server.js
+++ test/support/server.js
@@ -1,8 +1,8 @@
 // this is a test server to support tests which make requests
 
 var engine = require('engine.io');
-var server = engine.listen(process.env.ZUUL_PORT)
+var server = engine.listen(process.env.ZUUL_PORT);
 
-server.on('connection', function (socket) {
+server.on('connection', function(socket){
   socket.send('hi');
 });
