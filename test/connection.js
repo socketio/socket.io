78f1fd152face37b45ef7b0f194c22448aaa17dc
Naoyuki Kanezawa <naoyuki.kanezawa@gmail.com>
2014-02-06 05:03:25 +0900

    fix a namespace connection error

--- test/connection.js
+++ test/connection.js
@@ -9,4 +9,17 @@ describe('connection', function() {
       done();
     });
   });
+
+  it('should connect to a namespace after connection established', function(done) {
+    var manager = io.Manager();
+    var socket = manager.socket('/');
+    socket.on('connect', function(){
+      var foo = manager.socket('/foo');
+      foo.on('connect', function(){
+        foo.close();
+        socket.close();
+        done();
+      });
+    });
+  });
 });
