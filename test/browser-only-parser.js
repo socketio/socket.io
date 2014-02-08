1d3fc839d8a8af894f90f2471c71a09868079da6
Tony Kovanen <tonykovanen@hotmail.com>
2014-02-08 19:15:40 +0200

    Tests for encoding payloads as blobs

--- test/browser-only-parser.js
+++ test/browser-only-parser.js
@@ -17,6 +17,7 @@ var decode = parser.decodePacket;
 var encPayload = parser.encodePayload;
 var decPayload = parser.decodePayload;
 var encPayloadB = parser.encodePayloadAsArrayBuffer;
+var encPayloadBB = parser.encodePayloadAsBlob;
 var decPayloadB = parser.decodePayloadAsBinary;
 
 /**
@@ -50,7 +51,7 @@ describe('browser-only-parser', function () {
     });
   });
 
-  it('should encode binary contents as binary', function(done) {
+  it('should encode binary contents as binary (ArrayBuffer)', function(done) {
     var first = new Int8Array(5);
     for (var i = 0; i < first.length; i++) first[i] = i;
     var second = new Int8Array(4);
@@ -70,7 +71,7 @@ describe('browser-only-parser', function () {
     });
   });
 
-  it('should encode mixed binary and string contents as binary', function(done) {
+  it('should encode mixed binary and string contents as binary (ArrayBuffer)', function(done) {
     var first = new Int8Array(15);
     for (var i = 0; i < first.length; i++) first[i] = i;
 
@@ -89,4 +90,54 @@ describe('browser-only-parser', function () {
       });
     });
   });
+
+  it('should encode binary contents as binary (Blob)', function(done) {
+    var first = new Int8Array(5);
+    for (var i = 0; i < first.length; i++) first[i] = i;
+    var second = new Int8Array(4);
+    for (var i = 0; i < second.length; i++) second[i] = first.length + i;
+
+    encPayloadBB([{ type: 'message', data: first }, { type: 'message', data: second }], function(data) {
+      var fr = new FileReader();
+      fr.onload = function() {
+        decPayloadB(this.result, function(packet, index, total) {
+          var isLast = index + 1 == total;
+          expect(packet.type).to.eql('message');
+          if (!isLast) {
+            expect(new Int8Array(packet.data)).to.eql(first);
+          } else {
+            expect(new Int8Array(packet.data)).to.eql(second);
+            done();
+          }
+        });
+      };
+      fr.readAsArrayBuffer(data);
+    });
+  });
+
+  it('should encode mixed binary and string contents as binary (Blob)', function(done) {
+    var first = new Int8Array(5);
+    for (var i = 0; i < first.length; i++) first[i] = i;
+
+    encPayloadBB([ { type: 'message', data: first }, { type: 'message', data: 'hello' }, { type: 'close' } ], function(data) {
+      var fr = new FileReader();
+      fr.onload = function() {
+        decPayloadB(this.result, function(packet, index, total) {
+          console.log(packet);
+          if (index == 0) {
+            expect(packet.type).to.eql('message');
+            expect(new Int8Array(packet.data)).to.eql(first);
+          } else if (index == 1) {
+            expect(packet.type).to.eql('message');
+            expect(packet.data).to.eql('hello');
+          } else {
+            expect(packet.type).to.eql('close');
+            done();
+          }
+        });
+      };
+      fr.readAsArrayBuffer(data);
+    });
+  });
+
 });
