7466ebee3dce2496aa42606573b4a59a56611aeb
Guillermo Rauch <rauchg@gmail.com>
2014-02-11 14:19:37 -0800

    fix for old ies

--- test/connection.js
+++ test/connection.js
@@ -2,6 +2,8 @@ var expect = require('expect.js');
 var io = require('../');
 
 describe('connection', function() {
+  this.timeout(10000);
+
   it('should connect to localhost', function(done) {
     var socket = io();
     socket.on('hi', function(data){
