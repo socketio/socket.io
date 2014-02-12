0023aed4c3fe08e73fb19d00612962bee41f7f68
Grant Timmerman <granttimmerman@gmail.com>
2014-02-11 17:13:44 -0800

    Changed tabs to spaces, removed screenshot, and updated README

--- examples/chat/README.md
+++ examples/chat/README.md
@@ -1,12 +1,19 @@
-socket.io-chat
+socket.io chat
 ==============
 
-A simple chat demo for socket.io
+#####A simple chat demo for socket.io
 
 ###Quickstart
 ```
+$ npm install
 $ node app
 ```
 
-###Screenshot
-![screenshot](./screenshot.png)
+###Features
+ - Multiple users can join a chat room by entering a unique username on website load.
+ - Users can type chat messages to the chat room
+ - A notification is sent to all users when a user joins or leaves the chatroom
+
+###Uses
+ - Express
+ - jQuery
--- examples/chat/app.js
+++ examples/chat/app.js
@@ -10,13 +10,13 @@ console.log('Server listening at port %d', port);
 
 // Routing
 app.get('/', function (req, res) {
-	res.sendfile(__dirname + '/index.html');
+  res.sendfile(__dirname + '/index.html');
 });
 app.get('/main.js', function (req, res) {
-	res.sendfile(__dirname + '/main.js');
+  res.sendfile(__dirname + '/main.js');
 });
 app.get('/style.css', function (req, res) {
-	res.sendfile(__dirname + '/style.css');
+  res.sendfile(__dirname + '/style.css');
 });
 
 // Chatroom
@@ -26,36 +26,36 @@ var usernames = {};
 
 io.sockets.on('connection', function (socket) {
 
-	// when the client emits 'sendchat', this listens and executes
-	socket.on('sendchat', function (data) {
-		// we tell the client to execute 'updatechat' with 2 parameters
-		io.sockets.emit('updatechat', socket.username, data);
-	});
+  // when the client emits 'sendchat', this listens and executes
+  socket.on('sendchat', function (data) {
+    // we tell the client to execute 'updatechat' with 2 parameters
+    io.sockets.emit('updatechat', socket.username, data);
+  });
 
-	// when the client emits 'adduser', this listens and executes
-	socket.on('adduser', function(username) {
-		// we store the username in the socket session for this client
-		socket.username = username;
-		// add the client's username to the global list
-		usernames[username] = username;
-		// echo to client they've connected
-		socket.emit('updatechat', 'SERVER', 'you (' + username + ') have connected. ' + getNumberOfUsersString());
-		// echo globally (all clients) that a person has connected
-		socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected. ' + getNumberOfUsersString());
-	});
+  // when the client emits 'adduser', this listens and executes
+  socket.on('adduser', function(username) {
+    // we store the username in the socket session for this client
+    socket.username = username;
+    // add the client's username to the global list
+    usernames[username] = username;
+    // echo to client they've connected
+    socket.emit('updatechat', 'SERVER', 'you (' + username + ') have connected. ' + getNumberOfUsersString());
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected. ' + getNumberOfUsersString());
+  });
 
-	// when the user disconnects.. perform this
-	socket.on('disconnect', function() {
-		// remove the username from global usernames list
-		delete usernames[socket.username];
-		// echo globally that this client has left
-		socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected. ' + getNumberOfUsersString());
-	});
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function() {
+    // remove the username from global usernames list
+    delete usernames[socket.username];
+    // echo globally that this client has left
+    socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected. ' + getNumberOfUsersString());
+  });
 });
 
 // Gets a string that contains the number of users in the chatroom
 function getNumberOfUsersString () {
-	var numUsers = Object.keys(usernames).length;
-	var numUsersString = '<span class="log">(' + numUsers + ' ' + ((numUsers === 1) ? 'user' : 'users') + ' in chatroom)</span>';
-	return numUsersString;
+  var numUsers = Object.keys(usernames).length;
+  var numUsersString = '<span class="log">(' + numUsers + ' ' + ((numUsers === 1) ? 'user' : 'users') + ' in chatroom)</span>';
+  return numUsersString;
 }
