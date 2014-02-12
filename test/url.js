8a438a67a94c746f74f155283a77f43fb56ed7b8
Guillermo Rauch <rauchg@gmail.com>
2014-02-11 18:13:25 -0800

    fix url for ie6-8

--- test/url.js
+++ test/url.js
@@ -10,17 +10,16 @@ describe('url', function(){
     loc.hostname = 'woot.com';
     loc.protocol = 'https:';
     var parsed = url('/test', loc);
-    expect(parsed.hostname).to.be('woot.com');
-    expect(parsed.protocol).to.be('https:');
+    expect(parsed.host).to.be('woot.com');
+    expect(parsed.protocol).to.be('https');
   });
 
   it('works with no protocol', function(){
     loc.protocol = 'http:';
     var parsed = url('localhost:3000', loc);
-    expect(parsed.protocol).to.be('http:');
-    expect(parsed.hostname).to.be('localhost');
-    expect(parsed.host).to.be('localhost:3000');
+    expect(parsed.host).to.be('localhost');
     expect(parsed.port).to.be('3000');
+    expect(parsed.protocol).to.be('http');
   });
 
   it('ignores default ports for unique url ids', function(){
@@ -35,9 +34,9 @@ describe('url', function(){
     loc.protocol = 'http:';
     loc.hostname = 'woot.com';
 
-    expect(url('/woot').pathname).to.be('/woot');
-    expect(url('http://google.com').pathname).to.be('/');
-    expect(url('http://google.com/').pathname).to.be('/');
+    expect(url('/woot').path).to.be('/woot');
+    expect(url('http://google.com').path).to.be('/');
+    expect(url('http://google.com/').path).to.be('/');
   });
 
 });
