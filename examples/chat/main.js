b8f6d4054c5a3f637535cc968d44d09e724eaf87
Grant Timmerman <granttimmerman@gmail.com>
2014-02-11 21:11:58 -0800

    Added more friendly username input interface

--- examples/chat/main.js
+++ examples/chat/main.js
@@ -1,44 +1,75 @@
 $(function() {
   // Initialize varibles
+  var $window = $(window);
   var $messages = $('.messages'); // Messages area
   var $sendMessage = $('.sendMessage'); // Send message button
   var $inputMessage = $('.inputMessage'); // Input message input box
 
-  // Prompt for a username
-  var username = prompt("What's your name?");
+  var username;
+  addChatMessage('CHAT', 'Enter your username below...');
+  $inputMessage.attr('placeholder', 'username').focus();
+  $sendMessage.html('set username');
 
   var socket = io.connect();
 
-  // on connection to server
-  socket.on('connect', function() {
-    // call the server-side function 'add user' and send along one parameter (the username)
-    socket.emit('add user', username);
-  });
+  // Sets the client's username
+  function setUsername () {
+    username = $inputMessage.val().trim();
+    if (username) {
+      $inputMessage.val('');
+      $inputMessage.attr('placeholder', 'message');
+      $sendMessage.html('send');
 
-  // whenever the server emits 'update chat', update the chat body
-  socket.on('update chat', function (username, message) {
+      // Tell the server your username
+      socket.emit('add user', username);
+    }
+  }
+
+  // Sends a chat message
+  function sendMessage () {
+    var message = $inputMessage.val();
+    // if there is a non-empty message and a socket connection
+    if (message && socket) {
+      $inputMessage.val('');
+      // tell server to execute 'new message' and send along one parameter
+      socket.emit('new message', message);
+    }
+  }
+
+  // Adds the visual chat message to the message list
+  function addChatMessage (username, message) {
     var usernameDiv = '<div class="username">' + username + '</div>';
     var messageBodyDiv = '<div class="messageBody">' + message + '</div>';
     var messageDiv = '<div class="message">' + usernameDiv + messageBodyDiv + '</div>';
     $messages.append(messageDiv);
     $messages[0].scrollTop = $messages[0].scrollHeight;
-  });
+  }
 
-  // when the client clicks SEND
+  // When the client clicks SEND
   $sendMessage.click(function () {
-    var message = $inputMessage.val();
-    // if there is a non-empty message
-    if (message) {
-      $inputMessage.val('');
-      // tell server to execute 'new message' and send along one parameter
-      socket.emit('new message', message);
+    if (username) {
+      sendMessage();
+    } else {
+      setUsername();
     }
   });
 
-  // when the client hits ENTER on their keyboard
-  $(window).keypress(function (e) {
+  // When the client hits ENTER on their keyboard
+  $window.keypress(function (e) {
     if (e.which === 13) {
       $sendMessage.click();
     }
+  });
+
+  // Auto-focus the chat when a key is typed
+  $window.keydown(function (event) {
+    if (!(event.ctrlKey || event.metaKey || event.altKey)) {
+      $inputMessage.focus();
+    }
+  });
+
+  // Whenever the server emits 'update chat', update the chat body
+  socket.on('update chat', function (username, message) {
+    addChatMessage(username, message);
   });
 });
