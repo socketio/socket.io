3faca287a5d7059a76858f277f973bb176ea55a0
Guillermo Rauch <rauchg@gmail.com>
2014-02-24 16:54:38 -0800

    chat: forward 3000

--- examples/chat/index.js
+++ examples/chat/index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('../..')(server);
-var port = process.env.PORT || 8000;
+var port = process.env.PORT || 3000;
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
