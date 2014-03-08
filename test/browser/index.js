134897a1e00f357e58c937414485feed42c8fcc6
Tony Kovanen <tonykovanen@hotmail.com>
2014-03-09 01:01:17 +0200

    Cleaned up logic for deciding if blob tests should be run

--- test/browser/index.js
+++ test/browser/index.js
@@ -1,22 +1,9 @@
-var blobSupported = (function() {
-  try {
-    new Blob(['hi']);
-    return true;
-  } catch(e) {}
-  return false;
-})();
-
-/**
- * Create a blob builder even when vendor prefixes exist
- */
-
-var BlobBuilder = global.BlobBuilder || global.WebKitBlobBuilder || global.MSBlobBuilder || global.MozBlobBuilder;
-var blobBuilderSupported = !!BlobBuilder && !!BlobBuilder.prototype.append && !!BlobBuilder.prototype.getBlob;
+var Blob = require('blob');
 
 if (global.ArrayBuffer) {
   require('./arraybuffer.js');
 }
 
-if (blobSupported || blobBuilderSupported) {
+if (Blob) {
   require('./blob.js');
 }
