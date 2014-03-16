e303c080a9b4df74c33d4afd64cdd872f3143ad8
Tony Kovanen <tonykovanen@hotmail.com>
2014-03-16 14:18:44 +0200

    Added android as a browser for testing fixed tests accordingly

--- test/blob/index.js
+++ test/blob/index.js
@@ -3,6 +3,7 @@ var wsSupport = require('has-cors');
 require('./polling.js');
 var uagent = navigator.userAgent;
 var isOldSimulator = ~uagent.indexOf('iPhone OS 4') || ~uagent.indexOf('iPhone OS 5');
-if (wsSupport && !isOldSimulator) {
+var isAndroid = navigator.userAgent.match(/Android/i);
+if (wsSupport && !isOldSimulator && !isAndroid) {
   require('./ws.js');
 }
