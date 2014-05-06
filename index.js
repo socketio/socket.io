a9142147f7c7871f026a38f15633f16fd350523c
hallucynogenyc <hallucynogenyc@gmail.com>
2014-05-06 16:39:37 +0200

    Add autoprunning of empty rooms

--- index.js
+++ index.js
@@ -63,6 +63,9 @@ Adapter.prototype.del = function(id, room, fn){
   this.rooms[room] = this.rooms[room] || {};
   delete this.sids[id][room];
   delete this.rooms[room][id];
+  if(this.rooms[room].length == 0){
+    delete this.rooms[room];
+  }
   if (fn) process.nextTick(fn.bind(null, null));
 };
 
@@ -79,6 +82,10 @@ Adapter.prototype.delAll = function(id, fn){
     for (var room in rooms) {
       if (rooms.hasOwnProperty(room)) {
         delete this.rooms[room][id];
+      }
+
+      if(this.rooms[room].length == 0){
+        delete this.rooms[room];
       }
     }
   }
