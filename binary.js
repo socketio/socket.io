4e33d6a6c506c5b4f8ed76236b51119e5e0052c9
Kevin Roark <ker2143@columbia.edu>
2014-05-31 22:41:12 -0700

    Less indentation and a small optimization
    The optimization is the "else if" instead of "if" for checking
    array in _removeBlobs. No need to iterate over keys of a blob.

--- binary.js
+++ binary.js
@@ -18,32 +18,32 @@ exports.deconstructPacket = function(packet) {
     var buffers = [];
     var packetData = packet.data;
 
-    function deconstructBinPackRecursive(data) {
-        if (!data) return data;
+    function _deconstructPacket(data) {
+      if (!data) return data;
 
-        if ((global.Buffer && Buffer.isBuffer(data)) ||
-            (global.ArrayBuffer && data instanceof ArrayBuffer)) { // replace binary
-            var placeholder = {_placeholder: true, num: buffers.length};
-            buffers.push(data);
-            return placeholder;
-        } else if (isArray(data)) {
-            var newData = new Array(data.length);
-            for (var i = 0; i < data.length; i++) {
-                newData[i] = deconstructBinPackRecursive(data[i]);
-            }
-            return newData;
-        } else if ('object' == typeof data && !(data instanceof Date)) {
-            var newData = {};
-            for (var key in data) {
-                newData[key] = deconstructBinPackRecursive(data[key]);
-            }
-            return newData;
+      if ((global.Buffer && Buffer.isBuffer(data)) ||
+        (global.ArrayBuffer && data instanceof ArrayBuffer)) { // replace binary
+        var placeholder = {_placeholder: true, num: buffers.length};
+        buffers.push(data);
+        return placeholder;
+      } else if (isArray(data)) {
+        var newData = new Array(data.length);
+        for (var i = 0; i < data.length; i++) {
+          newData[i] = _deconstructPacket(data[i]);
         }
-        return data;
+        return newData;
+      } else if ('object' == typeof data && !(data instanceof Date)) {
+        var newData = {};
+        for (var key in data) {
+          newData[key] = _deconstructPacket(data[key]);
+        }
+        return newData;
+      }
+      return data;
     }
 
     var pack = packet;
-    pack.data = deconstructBinPackRecursive(packetData);
+    pack.data = _deconstructPacket(packetData);
     pack.attachments = buffers.length; // number of binary 'attachments'
     return {packet: pack, buffers: buffers};
 }
@@ -60,25 +60,25 @@ exports.deconstructPacket = function(packet) {
  exports.reconstructPacket = function(packet, buffers) {
     var curPlaceHolder = 0;
 
-    function reconstructBinPackRecursive(data) {
-        if (data && data._placeholder) {
-            var buf = buffers[data.num]; // appropriate buffer (should be natural order anyway)
-            return buf;
-        } else if (isArray(data)) {
-            for (var i = 0; i < data.length; i++) {
-                data[i] = reconstructBinPackRecursive(data[i]);
-            }
-            return data;
-        } else if (data && 'object' == typeof data) {
-            for (var key in data) {
-                data[key] = reconstructBinPackRecursive(data[key]);
-            }
-            return data;
+    function _reconstructPacket(data) {
+      if (data && data._placeholder) {
+        var buf = buffers[data.num]; // appropriate buffer (should be natural order anyway)
+        return buf;
+      } else if (isArray(data)) {
+        for (var i = 0; i < data.length; i++) {
+          data[i] = _reconstructPacket(data[i]);
         }
         return data;
+      } else if (data && 'object' == typeof data) {
+        for (var key in data) {
+          data[key] = _reconstructPacket(data[key]);
+        }
+        return data;
+      }
+      return data;
     }
 
-    packet.data = reconstructBinPackRecursive(packet.data);
+    packet.data = _reconstructPacket(packet.data);
     packet.attachments = undefined; // no longer useful
     return packet;
  }
@@ -95,7 +95,7 @@ exports.deconstructPacket = function(packet) {
 
 exports.removeBlobs = function(data, callback) {
 
-  function removeBlobsRecursive(obj, curKey, containingObject) {
+  function _removeBlobs(obj, curKey, containingObject) {
     if (!obj) return obj;
 
     // convert any blob
@@ -120,22 +120,20 @@ exports.removeBlobs = function(data, callback) {
       };
 
       fileReader.readAsArrayBuffer(obj); // blob -> arraybuffer
-    }
-
-    if (isArray(obj)) { // handle array
+    } else if (isArray(obj)) { // handle array
       for (var i = 0; i < obj.length; i++) {
-        removeBlobsRecursive(obj[i], i, obj);
+        _removeBlobs(obj[i], i, obj);
       }
     } else if (obj && 'object' == typeof obj && !isBuf(obj)) { // and object
       for (var key in obj) {
-        removeBlobsRecursive(obj[key], key, obj);
+        _removeBlobs(obj[key], key, obj);
       }
     }
   }
 
   var pendingBlobs = 0;
   var bloblessData = data;
-  removeBlobsRecursive(bloblessData);
+  _removeBlobs(bloblessData);
   if (!pendingBlobs) {
     callback(bloblessData);
   }
