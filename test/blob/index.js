8102833c27580f873de61145c9316327ccdf9bfe
Tony Kovanen <tonykovanen@hotmail.com>
2014-06-20 02:01:44 +0300

    Disable ws tests on IE11 because saucelabs breaks them. Let's run them in a VM until things get sorted out

--- test/blob/index.js
+++ test/blob/index.js
@@ -3,7 +3,8 @@ var wsSupport = require('has-cors');
 require('./polling.js');
 var uagent = navigator.userAgent;
 var isOldSimulator = ~uagent.indexOf('iPhone OS 4') || ~uagent.indexOf('iPhone OS 5');
+var isIE11 = !!navigator.userAgent.match(/Trident.*rv[ :]*11\./); // ws doesn't work at all in sauce labs
 var isAndroid = navigator.userAgent.match(/Android/i);
-if (wsSupport && !isOldSimulator && !isAndroid) {
+if (wsSupport && !isOldSimulator && !isAndroid && !isIE11) {
   require('./ws.js');
 }
