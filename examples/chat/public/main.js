ec305bd8df766e27ec316fdaa18e29587d74743b
Guillermo Rauch <rauchg@gmail.com>
2014-09-04 11:44:02 +0200

    examples: minor fix of escaping

--- examples/chat/public/main.js
+++ examples/chat/public/main.js
@@ -229,7 +229,7 @@ $(function() {
   socket.on('login', function (data) {
     connected = true;
     // Display the welcome message
-    var message = "Welcome to Socket.IO Chat &mdash; ";
+    var message = "Welcome to Socket.IO Chat – ";
     log(message, {
       prepend: true
     });
