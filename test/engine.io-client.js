bfc48ec4adb948d5a985df9dab5509dc598ec444
Roman Shtylman <shtylman@gmail.com>
2015-01-11 16:23:03 -0800

    fix default port detection when host is specified
    This fixes an issue when a full url is specified (i.e.
    http://service.com) the port would be the port from the page and not the
    uri string. This is easily exposed if using external engine.io servers
    on a page served up by localhost:3000 or similar from a development
    page.

--- test/engine.io-client.js
+++ test/engine.io-client.js
@@ -8,4 +8,24 @@ describe('engine.io-client', function () {
     expect(eio.protocol).to.be.a('number');
   });
 
+  it('should properly parse http uri without port', function() {
+    var server = eio('http://localhost');
+    expect(server.port).to.be('80');
+  });
+
+  it('should properly parse https uri without port', function() {
+    var server = eio('https://localhost');
+    expect(server.port).to.be('443');
+  });
+
+  it('should properly parse wss uri without port', function() {
+    var server = eio('wss://localhost');
+    expect(server.port).to.be('443');
+  });
+
+  it('should properly parse wss uri with port', function() {
+    var server = eio('wss://localhost:2020');
+    expect(server.port).to.be('2020');
+  });
+
 });
