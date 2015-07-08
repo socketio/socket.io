2807a2c34d1a70b8838f0cb8e719e0e160e37ed0
Guillermo Rauch <rauchg@gmail.com>
2015-07-08 10:24:37 -0700

    server: make error test pass consistently

--- test/server.js
+++ test/server.js
@@ -759,7 +759,8 @@ describe('server', function () {
     it('should trigger transport close before open for ws', function(done){
       var opts = { transports: ['websocket'] };
       var engine = listen(opts, function (port) {
-        var socket = new eioc.Socket('ws://invalidserver:%d'.s(port));
+        var url = 'ws://%s:%d'.s('0.0.0.50', port);
+        var socket = new eioc.Socket(url);
         socket.on('open', function(){
           done(new Error('Test invalidation'));
         });
