ff31233fcfb8faf2973bbf4a5249e922f85cb1ca
Guillermo Rauch <rauchg@gmail.com>
2016-01-07 17:24:29 -0800

    support: add fallback to `global` for nativescript [@ligaz]

--- support/browserify.js
+++ support/browserify.js
@@ -48,5 +48,6 @@ function build(fn){
 
 function glob(){
   return 'typeof self !== "undefined" ? self : '
-    + 'typeof window !== "undefined" ? window : {}';
+    + 'typeof window !== "undefined" ? window : '
+    + 'typeof global !== "undefined" ? global : {}';
 }
