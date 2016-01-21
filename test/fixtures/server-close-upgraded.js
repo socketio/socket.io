1a9856389821f335a557f3480c28e21a876be800
nkzawa <naoyuki.kanezawa@gmail.com>
2016-01-21 20:35:11 +0900

    add tests for graceful close

---
+++ test/fixtures/server-close-upgraded.js
@@ -0,0 +1,10 @@
+var eioc = require('engine.io-client');
+var listen = require('../common').listen;
+
+var engine = listen(function (port) {
+  var socket = new eioc.Socket('ws://localhost:' + port);
+  socket.on('upgrade', function() {
+    engine.httpServer.close();
+    engine.close();
+  });
+});
---
+++ test/fixtures/server-close.js
@@ -0,0 +1,10 @@
+var eioc = require('engine.io-client');
+var listen = require('../common').listen;
+
+var engine = listen(function (port) {
+  var socket = new eioc.Socket('ws://localhost:' + port);
+  socket.on('open', function() {
+    engine.httpServer.close();
+    engine.close();
+  });
+});
