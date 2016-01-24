1d07b1033994f84721d139b313631e1d29b47edf
Zhu Liang <zhuliangg11@gmail.com>
2016-01-24 20:46:38 +0800

    integrated babel into gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var babel = require("gulp-babel");
+
+gulp.task("default", function () {
+  return gulp.src("lib/*.js")
+    .pipe(babel())
+    .pipe(gulp.dest("dist"));
+});
 
 gulp.task('test', function(){
   return gulp.src('test/*.js', {read: false})
