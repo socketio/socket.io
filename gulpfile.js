94127a34656a3d959625eda12a17113a46786d0d
Zhu Liang <zhuliangg11@gmail.com>
2016-01-26 16:16:50 +0800

    added babel integration

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,15 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var file = require('gulp-file');
 var istanbul = require('gulp-istanbul');
-
+var babel = require("gulp-babel");
 var browserify = require('./support/browserify.js');
 
+// By default, individual js files are transformed by babel and exported to /dist
+gulp.task("babel", function () {
+  return gulp.src("lib/*.js")
+    .pipe(babel())
+    .pipe(gulp.dest("dist"));
+});
 
 gulp.task('build', function(){
   browserify(function(err, out){
