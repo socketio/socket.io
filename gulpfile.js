nus-fboa2016-si/engine.io

added babel under default gulp build task

gulpfile.js
@@ -2,17 +2,12 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var babel = require("gulp-babel");
 
-// By default, individual js files are transformed by babel and exported to /dist
-gulp.task("babel", function () {
-  return gulp.src(["lib/*.js","lib/transports/*.js"], { base: 'lib' })
-    .pipe(babel())
-    .pipe(gulp.dest("dist"));
-});
-
 var TESTS = 'test/*.js';
 var REPORTER = 'dot';
 
-gulp.task('test', function(
+gulp.task("default", ["babel"]);
+
+gulp.task('test', function(){
     return gulp.src(TESTS, {read: false})
     .pipe(mocha({
       timeout: 2000,
@@ -20,10 +15,17 @@ gulp.task('test', function(
       bail: true,
       globals: ['___eio', 'document']
     }))
-    .once('error', function () {
+    .once('error', function(){
       process.exit(1);
     })
-    .once('end', function () {
+    .once('end', function(){
       process.exit();
     });
+});
+
+// By default, individual js files are transformed by babel and exported to /dist
+gulp.task("build", function(){
+    return gulp.src(["lib/*.js","lib/transports/*.js"], { base: 'lib' })
+        .pipe(babel())
+        .pipe(gulp.dest("dist"));
 });

socketio/socket.io

gulpfile.js
@@ -2,17 +2,12 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var babel = require("gulp-babel");
 
-// By default, individual js files are transformed by babel and exported to /dist
-gulp.task("babel", function () {
-  return gulp.src(["lib/*.js","lib/transports/*.js"], { base: 'lib' })
-    .pipe(babel())
-    .pipe(gulp.dest("dist"));
-});
-
 var TESTS = 'test/*.js';
 var REPORTER = 'dot';
 
-gulp.task('test', function(
+gulp.task("default", ["babel"]);
+
+gulp.task('test', function(){
     return gulp.src(TESTS, {read: false})
     .pipe(mocha({
       timeout: 2000,
@@ -20,10 +15,17 @@ gulp.task('test', function(
       bail: true,
       globals: ['___eio', 'document']
     }))
-    .once('error', function () {
+    .once('error', function(){
       process.exit(1);
     })
-    .once('end', function () {
+    .once('end', function(){
       process.exit();
     });
+});
+
+// By default, individual js files are transformed by babel and exported to /dist
+gulp.task("babel", function(){
+    return gulp.src(["lib/*.js","lib/transports/*.js"], { base: 'lib' })
+        .pipe(babel())
+        .pipe(gulp.dest("dist"));
 });