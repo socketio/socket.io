867dd8024345a7a4f205b11c83631c98cfbcbd0b
Diga Widyaprana <digawp@gmail.com>
2016-03-13 16:42:47 +0800

    ESLint minor manual fix

--- test/fixtures/server-close.js
+++ test/fixtures/server-close.js
@@ -2,7 +2,7 @@ var server = require('http').createServer();
 var ioc = require('socket.io-client');
 var io = require('../..')(server);
 
-var srv = server.listen(function () {
+server.listen(function () {
   var socket = ioc('ws://localhost:' + server.address().port);
   socket.on('connect', function () {
     io.close();
