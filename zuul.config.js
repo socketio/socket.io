1e2323092b4bd7cfde21c2b467fa076b2b200792
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2016-12-23 13:58:28 +0100

    [chore] Update zuul config to speed up tests (#526)

--- zuul.config.js
+++ zuul.config.js
@@ -1,12 +1,32 @@
+'use strict';
 
-module.exports = {
+var browsers = require('socket.io-browsers');
+
+var zuulConfig = module.exports = {
   ui: 'mocha-bdd',
   server: './test/support/server.js',
-  tunnel: {
-    type: 'ngrok',
-    authtoken: '6Aw8vTgcG5EvXdQywVvbh_3fMxvd4Q7dcL2caAHAFjV',
-    proto: 'tcp'
-  },
   builder: 'zuul-builder-webpack',
-  webpack: require('./support/webpack.config.js')
+  webpack: require('./support/webpack.config.js'),
+
+  // test on localhost by default
+  local: true,
+
+  concurrency: 2, // ngrok only accepts two tunnels by default
+  // if browser does not sends output in 120s since last output:
+  // stop testing, something is wrong
+  browser_output_timeout: 120 * 1000,
+  browser_open_timeout: 60 * 4 * 1000,
+  // we want to be notified something is wrong asap, so no retry
+  browser_retries: 1
 };
+
+if (process.env.CI === 'true') {
+  zuulConfig.local = false;
+  zuulConfig.tunnel = {
+    type: 'ngrok',
+    bind_tls: true
+  };
+}
+
+var isPullRequest = process.env.TRAVIS_PULL_REQUEST && process.env.TRAVIS_PULL_REQUEST !== 'false';
+zuulConfig.browsers = isPullRequest ? browsers.pullRequest : browsers.all;
