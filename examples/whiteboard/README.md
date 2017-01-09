d283c9eb82c8ca45be1f2b18f320459ec1763a5a
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2017-01-09 18:03:25 +0100

    Implement whitebaord example

---
+++ examples/whiteboard/README.md
@@ -0,0 +1,17 @@
+
+# Socket.IO Collaborative Whiteboard
+
+A simple collaborative whiteboard for socket.io
+
+## How to use
+
+```
+$ npm i && npm start
+```
+
+And point your browser to `http://localhost:3000`. Optionally, specify
+a port by supplying the `PORT` env variable.
+
+## Features
+
+- draw on the whiteboard and all other users will see you drawings live
---
+++ examples/whiteboard/index.js
@@ -0,0 +1,16 @@
+
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+function onConnection(socket){
+  socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+}
+
+io.on('connection', onConnection);
+
+http.listen(port, () => console.log('listening on port ' + port));
