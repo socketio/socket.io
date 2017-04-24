f44256c523ac8f0219ffd20d8fd73a0e2b445f28
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2017-04-24 23:20:51 +0200

    [feat] Move binary detection to the parser (#66)

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 
 var debug = require('debug')('socket.io-parser');
 var Emitter = require('component-emitter');
+var hasBin = require('has-binary2');
 var binary = require('./binary');
 var isBuf = require('./is-buffer');
 
@@ -123,6 +124,10 @@ function Encoder() {}
  */
 
 Encoder.prototype.encode = function(obj, callback){
+  if ((obj.type === exports.EVENT || obj.type === exports.ACK) && hasBin(obj.data)) {
+    obj.type = obj.type === exports.EVENT ? exports.BINARY_EVENT : exports.BINARY_ACK;
+  }
+
   debug('encoding packet %j', obj);
 
   if (exports.BINARY_EVENT === obj.type || exports.BINARY_ACK === obj.type) {
