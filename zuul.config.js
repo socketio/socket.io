11f3fddc6d842621e1690250e50467253a7736c3
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2017-05-19 08:43:31 +0200

    [test] Launch browser tests on localhost by default (#571)

--- zuul.config.js
+++ zuul.config.js
@@ -1,12 +1,17 @@
 
-module.exports = {
+var zuulConfig = module.exports = {
   ui: 'mocha-bdd',
   server: './test/support/server.js',
-  tunnel: {
-    type: 'ngrok',
-    authtoken: '6Aw8vTgcG5EvXdQywVvbh_3fMxvd4Q7dcL2caAHAFjV',
-    proto: 'tcp'
-  },
+  local: true, // test on localhost by default
   builder: 'zuul-builder-webpack',
   webpack: require('./support/webpack.config.js')
 };
+
+if (process.env.CI === 'true') {
+  zuulConfig.local = false;
+  zuulConfig.tunnel = {
+    type: 'ngrok',
+    authtoken: '6Aw8vTgcG5EvXdQywVvbh_3fMxvd4Q7dcL2caAHAFjV',
+    proto: 'tcp'
+  };
+}
