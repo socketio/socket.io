6a16ea119280a02029618544d44eb515f7f2d076
JinHyuk Kim <tk5641@naver.com>
2018-04-30 14:39:40 +0900

    [test] Remove unnecessary assertion in test case (#556)

--- test/engine.io.js
+++ test/engine.io.js
@@ -20,7 +20,6 @@ describe('engine', function () {
   });
 
   it('should be the same version as client', function () {
-    expect(eio.protocol).to.be.a('number');
     var version = require('../package').version;
     expect(version).to.be(require('engine.io-client/package').version);
   });
