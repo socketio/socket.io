f13d43f00d3e74d8ebba04ab4524c883bdf96d6b
Maxime Kjaer <maxime.kjaer@gmail.com>
2021-03-23 15:16:22 +0100

    Remove unnecessary type annotations in examples
    Typed events in socket.io 4.0 remove the need for writing type
    annotations in callbacks of reserved events.

--- examples/typescript/server.ts
+++ examples/typescript/server.ts
@@ -1,8 +1,8 @@
-import { Server, Socket } from "socket.io";
+import { Server } from "socket.io";
 
 const io = new Server(8080);
 
-io.on("connect", (socket: Socket) => {
+io.on("connect", (socket) => {
     console.log(`connect ${socket.id}`);
 
     socket.on("ping", (cb) => {
