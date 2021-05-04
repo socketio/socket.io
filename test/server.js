ad5306aeaedf06ac7a49f791e1b76e55c35a564e
Branislav Katreniak <katreniak@gmail.com>
2021-05-04 08:35:23 +0200

    perf(websocket): fix write back-pressure (#618)
    This change should reduce memory usage when many packets are emitted to
    many clients in a burst.
    
    Co-authored-by: Branislav Katreniak <bkatreniak@slido.com>

--- test/server.js
+++ test/server.js
@@ -1681,7 +1681,9 @@ describe("server", () => {
           conn.send("a");
           conn.send("b");
           conn.send("c");
-          conn.close();
+          setTimeout(() => {
+            conn.close();
+          }, 50);
         });
 
         socket.on("open", () => {
