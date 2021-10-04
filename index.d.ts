908fbc4f4c40e4aabb31212f9325e26ed25785ef
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2021-10-04 08:01:23 +0200

    chore: add removeListener typings
    Related: https://github.com/DefinitelyTyped/DefinitelyTyped/pull/55968

---
+++ index.d.ts
@@ -0,0 +1,18 @@
+interface Emitter<Event = string> {
+    on(event: Event, listener: Function): Emitter;
+    once(event: Event, listener: Function): Emitter;
+    off(event?: Event, listener?: Function): Emitter;
+    emit(event: Event, ...args: any[]): Emitter;
+    listeners(event: Event): Function[];
+    hasListeners(event: Event): boolean;
+    removeListener(event?: Event, listener?: Function): Emitter;
+    removeEventListener(event?: Event, listener?: Function): Emitter;
+    removeAllListeners(event?: Event): Emitter;
+}
+
+declare const Emitter: {
+    (obj?: object): Emitter;
+    new (obj?: object): Emitter;
+};
+
+export = Emitter;
