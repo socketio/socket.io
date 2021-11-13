27936285eaac6213d78c1abe06708b41429278f6
Luigi Pinca <luigipinca@gmail.com>
2021-11-13 06:33:06 +0100

    chore: make postcompile.sh script work on macOS (#681)
    On macOS the script was interpreted as the value of the `-i` option. It
    will now use an empty extension for the `-i` option and the `-e` option
    to specify the script.

--- postcompile.sh
+++ postcompile.sh
@@ -1,8 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 cp ./support/package.cjs.json ./build/cjs/package.json
 cp ./support/package.esm.json ./build/esm/package.json
 
 cp -r ./build/esm/ ./build/esm-debug/
 
-sed -i '/debug(/d' ./build/esm/*.js ./build/esm/**/*.js
+if [ "${OSTYPE:0:6}" = darwin ]; then
+  sed -i '' -e '/debug(/d' ./build/esm/*.js ./build/esm/**/*.js
+else
+  sed -i -e '/debug(/d' ./build/esm/*.js ./build/esm/**/*.js
+fi
