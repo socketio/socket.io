572133a58ddbfa83f3c8802360666e71617e7ea3
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2022-04-22 23:09:24 +0200

    docs(examples): update example with webpack

---
+++ examples/webpack-build/index.js
@@ -0,0 +1,15 @@
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:3000");
+
+socket.on("connect", () => {
+  console.log(`connect ${socket.id}`);
+});
+
+socket.on("connect_error", (err) => {
+  console.log(`connect_error due to ${err.message}`);
+});
+
+socket.on("disconnect", (reason) => {
+  console.log(`disconnect due to ${reason}`);
+});
---
+++ examples/webpack-build/webpack.config.js
@@ -0,0 +1,7 @@
+export default {
+  entry: "./index.js",
+  mode: "production",
+  output: {
+    filename: "bundle.js",
+  },
+};
