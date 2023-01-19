6c27b8b0a69d8bf1033655f432cc74bfc1042db9
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2023-01-19 11:53:53 +0100

    test: add test with socket.disconnect(true)
    Related: https://github.com/socketio/engine.io/commit/a65a047526401bebaa113a8c70d03f5d963eaa54

--- test/socket.ts
+++ test/socket.ts
@@ -1,6 +1,12 @@
 import fs = require("fs");
 import { join } from "path";
-import { createClient, getPort, success, successFn } from "./support/util";
+import {
+  createClient,
+  createPartialDone,
+  getPort,
+  success,
+  successFn,
+} from "./support/util";
 import { Server } from "..";
 import expect from "expect.js";
 
@@ -1038,6 +1044,31 @@ describe("socket", () => {
 
         socket.emit("my-event", "123");
       });
+    });
+
+    it("should disconnect all namespaces when calling disconnect(true)", (done) => {
+      const io = new Server(0);
+      io.of("/foo");
+      io.of("/bar");
+
+      const socket1 = createClient(io, "/", {
+        transports: ["websocket"],
+      });
+      const socket2 = createClient(io, "/foo");
+      const socket3 = createClient(io, "/bar");
+
+      io.of("/bar").on("connection", (socket) => {
+        socket.disconnect(true);
+      });
+
+      const partialDone = createPartialDone(
+        3,
+        successFn(done, io, socket1, socket2, socket3)
+      );
+
+      socket1.on("disconnect", partialDone);
+      socket2.on("disconnect", partialDone);
+      socket3.on("disconnect", partialDone);
     });
   });
 });
