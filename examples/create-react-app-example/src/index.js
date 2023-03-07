5f5982658b27b4e27ac32a6dea0f90ef991d8f2b
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2023-03-07 08:30:46 +0100

    docs(examples): upgrade to React 18
    Reference: https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html

--- examples/create-react-app-example/src/index.js
+++ examples/create-react-app-example/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container)
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
