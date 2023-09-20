ccbb4c077375ec2cb0d1c472a902e52f1d9a7dfc
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2023-09-20 12:45:04 +0200

    docs: add example with connection state recovery

---
+++ examples/connection-state-recovery-example/README.md
@@ -0,0 +1,25 @@
+# Example with connection state recovery
+
+This example shows how to use the [Connection state recovery feature](https://socket.io/docs/v4/connection-state-recovery).
+
+![Video of the example](assets/csr.gif)
+
+## How to use
+
+```shell
+# choose your module syntax (either ES modules or CommonJS)
+$ cd esm/
+
+# install the dependencies
+$ npm i
+
+# start the server
+$ node index.js
+```
+
+And point your browser to `http://localhost:3000`.
+
+You can also run this example directly in your browser on:
+
+- [CodeSandbox](https://codesandbox.io/p/sandbox/github/socketio/socket.io/tree/main/examples/connection-state-recovery-example/esm?file=index.js)
+- [StackBlitz](https://stackblitz.com/github/socketio/socket.io/tree/main/examples/connection-state-recovery-example/esm?file=index.js)
---
+++ examples/connection-state-recovery-example/cjs/index.js
@@ -0,0 +1,53 @@
+const { readFile } = require("node:fs/promises");
+const { createServer } = require("node:http");
+const { Server } = require("socket.io");
+
+const httpServer = createServer(async (req, res) => {
+  if (req.url !== "/") {
+    res.writeHead(404);
+    res.end("Not found");
+    return;
+  }
+  // reload the file every time
+  const content = await readFile("index.html");
+  const length = Buffer.byteLength(content);
+
+  res.writeHead(200, {
+    "Content-Type": "text/html",
+    "Content-Length": length,
+  });
+  res.end(content);
+});
+
+const io = new Server(httpServer, {
+  connectionStateRecovery: {
+    // the backup duration of the sessions and the packets
+    maxDisconnectionDuration: 2 * 60 * 1000,
+    // whether to skip middlewares upon successful recovery
+    skipMiddlewares: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`connect ${socket.id}`);
+
+  if (socket.recovered) {
+    console.log("recovered!");
+    console.log("socket.rooms:", socket.rooms);
+    console.log("socket.data:", socket.data);
+  } else {
+    console.log("new connection");
+    socket.join("sample room");
+    socket.data.foo = "bar";
+  }
+
+  socket.on("disconnect", (reason) => {
+    console.log(`disconnect ${socket.id} due to ${reason}`);
+  });
+});
+
+setInterval(() => {
+  io.emit("ping", new Date().toISOString());
+}, 1000);
+
+httpServer.listen(3000);
