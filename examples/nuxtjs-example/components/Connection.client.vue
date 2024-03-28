c42aa6bbe1fd9ce79c8ced4d0c569f5f7b6f1dde
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2024-03-28 17:39:49 +0100

    docs: add example with Nuxt
    Related: https://socket.io/how-to/use-with-nuxt

---
+++ examples/nuxtjs-example/components/Connection.client.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { socket } from "./socket";
+
+const isConnected = ref(false);
+const transport = ref("N/A");
+
+if (socket.connected) {
+  onConnect();
+}
+
+function onConnect() {
+  isConnected.value = true;
+  transport.value = socket.io.engine.transport.name;
+
+  socket.io.engine.on("upgrade", (rawTransport) => {
+    transport.value = rawTransport.name;
+  });
+}
+
+function onDisconnect() {
+  isConnected.value = false;
+  transport.value = "N/A";
+}
+
+socket.on("connect", onConnect);
+socket.on("disconnect", onDisconnect);
+
+onBeforeUnmount(() => {
+  socket.off("connect", onConnect);
+  socket.off("disconnect", onDisconnect);
+});
+
+</script>
+
+<template>
+<div>
+  <p>Status: {{ isConnected ? "connected" : "disconnected" }}</p>
+  <p>Transport: {{ transport }}</p>
+</div>
+</template>
---
+++ examples/nuxtjs-example/server/plugins/socket.io.ts
@@ -0,0 +1,37 @@
+import type { NitroApp } from "nitropack";
+import { Server as Engine } from "engine.io";
+import { Server } from "socket.io";
+import { defineEventHandler } from "h3";
+
+export default defineNitroPlugin((nitroApp: NitroApp) => {
+  const engine = new Engine();
+  const io = new Server();
+
+  io.bind(engine);
+
+  io.on("connection", (socket) => {
+    // ...
+  });
+
+  nitroApp.router.use("/socket.io/", defineEventHandler({
+    handler(event) {
+      engine.handleRequest(event.node.req, event.node.res);
+      event._handled = true;
+    },
+    websocket: {
+      open(peer) {
+        const nodeContext = peer.ctx.node;
+        const req = nodeContext.req;
+
+        // @ts-expect-error private method
+        engine.prepare(req);
+
+        const rawSocket = nodeContext.req.socket;
+        const websocket = nodeContext.ws;
+
+        // @ts-expect-error private method
+        engine.onWebSocket(req, rawSocket, websocket);
+      }
+    }
+  }));
+});
