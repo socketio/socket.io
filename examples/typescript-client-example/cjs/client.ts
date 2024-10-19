175a2c58c1bc37eb9b87f87df47e1f9388b01d55
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2024-10-19 06:52:08 +0200

    fix(eio-client/types): remove ws type from .d.ts file
    Before this change, the following error would be thrown when compiling
    with TypeScript:
    
    ```
    node_modules/engine.io-client/build/esm/transports/websocket.node.d.ts:12:101 - error TS1340: Module 'ws' does not refer to a type, but is used as a type here. Did you mean 'typeof import('ws')'?
    
    12     createSocket(uri: string, protocols: string | string[] | undefined, opts: Record<string, any>): import("ws");
                                                                                                           ~~~~~~~~~~~~
    ```
    
    This behavior was introduced in [1], included in version `6.6.0`.
    
    The return type is forced as `any`, so that the `@types/ws` dependency
    is optional.
    
    [1]: https://github.com/socketio/socket.io/commit/f4d898ee9652939a4550a41ac0e8143056154c0a
    
    Related: https://github.com/socketio/socket.io/issues/5202

---
+++ examples/typescript-client-example/cjs/client.ts
@@ -0,0 +1,30 @@
+import { io, type Socket } from "socket.io-client";
+
+interface ServerToClientEvents {
+    hello: (val: string) => void;
+}
+
+interface ClientToServerEvents {
+    ping: (cb: () => void) => void;
+}
+
+const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io("ws://localhost:8080/");
+
+socket.on("connect", () => {
+    console.log(`connect ${socket.id}`);
+});
+
+socket.on("hello", (val) => {
+    console.log(`got ${val}`);
+});
+
+socket.on("disconnect", () => {
+    console.log(`disconnect`);
+});
+
+setInterval(() => {
+    const start = Date.now();
+    socket.emit("ping", () => {
+        console.log(`pong (latency: ${Date.now() - start} ms)`);
+    });
+}, 1000);
