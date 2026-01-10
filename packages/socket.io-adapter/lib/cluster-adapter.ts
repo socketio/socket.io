da04267ffc7b0903ca91f2fccb80e56246d13328
seungeonchoi <chou610@gmail.com>
2026-01-10 14:54:21 +0900

    fix: cleanup pending acks on timeout to prevent memory leak (#5442)
    When using `emitWithAck` with a timeout, if clients didn't respond
    and the timeout triggers, the ack callbacks remained in `socket.acks`
    Map indefinitely, causing a memory leak.
    
    Related: https://github.com/socketio/socket.io/issues/4984

--- packages/socket.io-adapter/lib/cluster-adapter.ts
+++ packages/socket.io-adapter/lib/cluster-adapter.ts
@@ -508,7 +508,7 @@ export abstract class ClusterAdapter extends Adapter {
       }, opts.flags!.timeout);
     }
 
-    super.broadcastWithAck(packet, opts, clientCountCallback, ack);
+    return super.broadcastWithAck(packet, opts, clientCountCallback, ack);
   }
 
   override async addSockets(opts: BroadcastOptions, rooms: Room[]) {
